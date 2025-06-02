7094857ed44167d04b621b2cac414926a5de7420
fercaritafeliz <fercienciaypagos@gmail.com>
2025-06-02 00:09:34 -0600

    agregando red neuronal

--- src/sections/website/views/HomeView.tsx
+++ src/sections/website/views/HomeView.tsx
@@ -1,13 +1,14 @@
 import Pricing from "../home/Pricing.tsx";
 import Resenas from "../home/Reseñas.tsx";
 import Hero from "../home/Hero.tsx"
-
+import NeuralNetworkChart from "../home/RedesNeuronales.tsx"
 export default function HomeView() {
   return (
     <>        
         <Hero/>
         <Pricing/>
         <Resenas/>
+        <NeuralNetworkChart/>
     </>
   )
 }
