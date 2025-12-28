4e37a27726f7c6b9dee898ea6637971fb4d6ad6e
fercaritafeliz <fercienciaypagos@gmail.com>
2025-12-28 00:22:58 -0600

    otrovlv

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,13 +1,9 @@
 import LoginView from '../../sections/auth/LoginView.tsx'
-import { Title } from 'react-head';
 
 export default function LoginPage() {
   return (
     <>
-        <Title>
-            <title>Iniciar sesión</title>
-        </Title>
-
+        <title>Iniciar sesión</title>
         <LoginView/> 
     </>
   )
--- src/pages/website/HomePage.tsx
+++ src/pages/website/HomePage.tsx
@@ -1,11 +1,8 @@
-import { Title } from 'react-head';
 import HomeView from "../../sections/website/views/HomeView.tsx";
 export default function HomePage() {
   return (
     <>
-      <Title>
-        <title>Bienvenido | Mistli - Soluciones en Ingeniería</title>
-      </Title>
+      <title>Bienvenido | Mistli - Soluciones en Ingeniería</title>
       <HomeView/>
     </>
   )
