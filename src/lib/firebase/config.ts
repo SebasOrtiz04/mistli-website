b78aecc3369c877b55eda7e308abcdea085205d5
fercaritafeliz <fercienciaypagos@gmail.com>
2026-03-21 04:30:02 -0600

    agregando cambios para que funcione el logueo bien

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAGnqTN7FnHZIP6Q4MvkFCRHdqPdAIJmwE",
-  authDomain: "ohtli-mistli.firebaseapp.com",
-  projectId: "ohtli-mistli",
-  storageBucket: "ohtli-mistli.firebasestorage.app",
-  messagingSenderId: "375334609512",
-  appId: "1:375334609512:web:b745d4b39034f7f0fb51dd",
-  measurementId: "G-MRSNW080E0"
+  apiKey: "AIzaSyCiyNu-HcCKr1_hocCgnV8BxYAT3LotVN0",
+  authDomain: "mistli.firebaseapp.com",
+  projectId: "mistli",
+  storageBucket: "mistli.firebasestorage.app",
+  messagingSenderId: "655938664799",
+  appId: "1:655938664799:web:9d4ad3db9de2acf7f6deea",
+  measurementId: "G-0Y6XT2G6PK"
 };
   export default firebaseConfig
