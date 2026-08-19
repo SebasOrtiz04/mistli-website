3c79207cb39210a0894697f777638492b706f867
fercaritafeliz <fercienciaypagos@gmail.com>
2026-08-19 01:31:25 -0600

    agregando paddings a ultimas secciones

--- src/sections/website/home/Frontend.tsx
+++ src/sections/website/home/Frontend.tsx
@@ -16,7 +16,7 @@ const FrontendDevSection: React.FC<FrontendDevSectionProps> = ({ className = '' 
 
 
   return (
-    <section id='frontend' className={`w-full max-w-6xl mx-auto flex md:h-[100vh] ${className}`}>
+    <section id='frontend' className={`w-full max-w-7xl mx-auto flex px-6 py-28 md:h-[100vh] ${className}`}>
       <div className="grid md:grid-cols-2 gap-14 items-center">
         {/* Left – UI preview */}
         <div className="relative bg-white rounded-2xl shadow-xl border border-gray-100 p-6">
